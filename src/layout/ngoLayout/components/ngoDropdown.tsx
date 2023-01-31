00d12af927c501487b074a73b0a8a1c2e8abdcbd
areeburrub <areeburrub@gmail.com>
2023-01-31 09:52:21 +0530

    added: consignment to tracking page

---
+++ src/layout/ngoLayout/components/ngoDropdown.tsx
@@ -0,0 +1,163 @@
+import React, { useEffect, useRef, useState } from "react";
+import { useRouter } from "next/router";
+
+
+
+const NgoDropdown = () => {
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const menuRef = useRef(null);
+
+  useEffect(() => {
+
+    function handleClickOutside(event: { target: any }) {
+      // @ts-ignore
+      if (menuRef.current && !menuRef.current.contains<any>(event.target)) {
+        setIsOpen(false);
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+
+  const router = useRouter();
+
+
+  return (
+    <div className="" ref={menuRef}>
+      <div className="w-full flex flex-col md:hidden items-center justify-center my-8">
+        <div className="flex flex-col md:flex-row mx-6 font-semibold md:gap-2 gap-8 text-2xl md:text-lg items-center">
+          <a
+            className="text-gray-700 transition-colors duration-300 transform dark:text-gray-200 hover:text-green-500 dark:hover:text-blue-400 md:mx-4 md:my-0 cursor-pointer"
+            onClick={() => {
+              router.push("/dashboard");
+            }}
+          >
+            Dashboard
+          </a>
+          <a
+            className="text-gray-700 transition-colors duration-300 transform dark:text-gray-200 hover:text-green-500 dark:hover:text-blue-400 md:mx-4 md:my-0 cursor-pointer"
+          >
+            Logout
+          </a>
+
+          <a
+            href="#"
+            className="flex items-center p-3 w-full px-3 text-sm text-gray-600 transition-colors duration-300 transform dark:text-gray-300 bg-gray-200 dark:bg-gray-700 space-x-3 dark:hover:text-white rounded-xl"
+          >
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img
+              className="flex-shrink-0 object-cover mx-1 rounded-full w-11 h-11 bg-slate-400 overflow-clip"
+              src={"https://api.dicebear.com/5.x/shapes/svg"}
+            />
+            <div className="mx-1">
+              <h1 className="text-sm font-medium text-gray-600 dark:text-gray-400">
+                Welcome,
+              </h1>
+              <p className="text-base font-semibold text-gray-800 dark:text-gray-200">
+                Sunshine NGO
+              </p>
+            </div>
+          </a>
+        </div>
+      </div>
+
+      <div className="relative hidden w-9 h-9 md:flex flex-col m-auto">
+        <span
+          className="relative z-10 flex items-center justify-center text-sm text-gray-600 bg-white border border-transparent  dark:text-white dark:bg-gray-800  w-fit outline outline-offset-1 outline-green-500 rounded-full"
+          onClick={() => {
+            setIsOpen(!isOpen);
+          }}
+        >
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            className="w-9 h-9 m-auto rounded-full object-cover overflow-clip"
+            src={"https://api.dicebear.com/5.x/shapes/svg"}
+          />
+        </span>
+
+        <div
+          className={`absolute right-0 z-20 w-56 py-2 mt-14 -mr-2 overflow-hidden bg-white rounded-md shadow-xl dark:bg-gray-700 ${
+            isOpen ? "block" : "hidden"
+          }`}
+        >
+          <div
+            className="flex items-center p-3 w-full-mt-2 text-sm text-gray-600 transition-colors duration-300 transform dark:text-gray-300 hover:bg-gray-100 dark:bg-gray-600 dark:hover:text-white"
+          >
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img
+              className="flex-shrink-0 object-cover mx-1 rounded-full w-9 h-9"
+              src={"https://api.dicebear.com/5.x/shapes/svg"}
+            />
+            <div className="mx-1">
+              <h1 className="text-sm font-semibold text-gray-700 dark:text-gray-200">
+                {"Sunshine NGO"}
+              </h1>
+              <p className="text-xs text-gray-500 dark:text-gray-400">
+                contact@sunshine.org
+              </p>
+            </div>
+          </div>
+
+          <hr className="border-gray-200 dark:border-gray-700 " />
+
+          <button
+            onClick={() => {
+              router.push("#");
+              setIsOpen(false);
+            }}
+            className="flex items-center p-3 w-full text-sm text-gray-600 capitalize transition-colors duration-300 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+          >
+            <svg
+              className="w-5 h-5 mx-1"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M7 8C7 5.23858 9.23858 3 12 3C14.7614 3 17 5.23858 17 8C17 10.7614 14.7614 13 12 13C9.23858 13 7 10.7614 7 8ZM12 11C13.6569 11 15 9.65685 15 8C15 6.34315 13.6569 5 12 5C10.3431 5 9 6.34315 9 8C9 9.65685 10.3431 11 12 11Z"
+                fill="currentColor"
+              ></path>
+              <path
+                d="M6.34315 16.3431C4.84285 17.8434 4 19.8783 4 22H6C6 20.4087 6.63214 18.8826 7.75736 17.7574C8.88258 16.6321 10.4087 16 12 16C13.5913 16 15.1174 16.6321 16.2426 17.7574C17.3679 18.8826 18 20.4087 18 22H20C20 19.8783 19.1571 17.8434 17.6569 16.3431C16.1566 14.8429 14.1217 14 12 14C9.87827 14 7.84344 14.8429 6.34315 16.3431Z"
+                fill="currentColor"
+              ></path>
+            </svg>
+
+            <span className="mx-1">Dashboard</span>
+          </button>
+          <hr className="border-gray-200 dark:border-gray-700 " />
+
+          <button
+            onClick={() => {
+              setIsOpen(false);
+            }}
+            className="flex items-center p-3 w-full text-sm text-gray-600 capitalize transition-colors duration-300 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-red-600 dark:hover:text-white"
+          >
+            <svg
+              className="w-5 h-5 mx-1"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M19 21H10C8.89543 21 8 20.1046 8 19V15H10V19H19V5H10V9H8V5C8 3.89543 8.89543 3 10 3H19C20.1046 3 21 3.89543 21 5V19C21 20.1046 20.1046 21 19 21ZM12 16V13H3V11H12V8L17 12L12 16Z"
+                fill="currentColor"
+              ></path>
+            </svg>
+
+            <span className="mx-1">Sign Out</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NgoDropdown;
--- src/pages/tracking.tsx
+++ src/pages/tracking.tsx
@@ -1,95 +1,137 @@
-import {TrackingView} from "@/components/trackingView";
-import React, {useState} from "react";
-import {TabsLayout} from "layout";
+import { TrackingView } from "@/components/trackingView";
+import React, { useState } from "react";
+import { TabsLayout } from "layout";
 
 function TrackingPage() {
     const [searchStatus, setSearch] = useState(false);
     const [loading, setLoading] = useState(false);
     return (
-        <>
-            <div className={"container mx-auto flex flex-col justify-center items-center"}>
+      <>
+          <div
+            className={
+                "container mx-auto flex flex-col items-center justify-center"
+            }
+          >
+              <section className="bg-white dark:bg-gray-900">
+                  <div
+                    className={`${
+                      searchStatus ? "" : ""
+                    } mx-auto max-w-screen-xl py-8 px-4 text-center lg:py-16 lg:px-12`}
+                  >
+                      <p
+                        className="text-4xl font-extrabold leading-loose leading-none tracking-tight text-gray-900 dark:text-white md:mb-4 md:text-5xl lg:text-6xl">
+                          Realtime Tracking
+                      </p>
 
-                <section className="bg-white dark:bg-gray-900">
-                    <div
-                        className={`${searchStatus ? "" : ""} py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12`}>
-                        <p className="md:mb-4 text-4xl font-extrabold leading-loose tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Realtime
-                            Tracking</p>
-
-                        <p className="md:mb-4 text-4xl font-extrabold leading-loose tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
-                            <strong className={"text-green-600"}>Globally</strong></p>
-                        <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400"></p>
-                        <div
-                            className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
-                            <a href="#"
-                               className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-green-600 hover:bg-green-800 focus:ring-4 focus:ring-green-300 dark:focus:ring-green-900">
-                                Learn more
-                                <svg className="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
-                                     xmlns="http://www.w3.org/2000/svg">
-                                    <path fillRule="evenodd"
-                                          d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                                          clipRule="evenodd"></path>
-                                </svg>
-                            </a>
-                        </div>
-                        <div className="px-4 mx-auto text-center md:max-w-screen-md lg:max-w-screen-lg lg:px-36">
-                            {!searchStatus && (
-                                <form className={"mt-7"} onSubmit={(e) => {
-                                    e.preventDefault();
-                                }
-                                }>
-                                    <div className="mb-6">
-                                        <label htmlFor="text"
-                                               className="block mb-2 text-sm font-medium text-gray-500 dark:text-white">Waybill
-                                            Number</label>
-                                        <div className={"flex space-x-6 text-gray-500 justify-center items-center"}>
-                                            <input type="number" id="email"
-                                                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500 placeholder-gray-400"
-                                                   placeholder="1234455677"/>
-                                        </div>
-                                        <button onClick={() => {
-                                            setLoading(true);
-                                            setTimeout(() => {
-                                                setSearch(true);
-                                                setLoading(false);
-                                            }, 1500)
-                                        }}
-                                                className="inline-flex my-5 justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-green-600 hover:bg-green-800 focus:ring-4 focus:ring-green-300 dark:focus:ring-green-900">
-                                            Track
-                                            <svg className="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
-                                                 xmlns="http://www.w3.org/2000/svg">
-                                                <path fillRule="evenodd"
-                                                      d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                                                      clipRule="evenodd"></path>
-                                            </svg>
-                                        </button>
+                      <p
+                        className="text-4xl font-extrabold leading-loose leading-none tracking-tight text-gray-900 dark:text-white md:mb-4 md:text-5xl lg:text-6xl">
+                          <strong className={"text-green-600"}>Globally</strong>
+                      </p>
+                      <p
+                        className="mb-8 text-lg font-normal text-gray-500 dark:text-gray-400 sm:px-16 lg:text-xl xl:px-48"></p>
+                      <div
+                        className="mb-8 flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4 lg:mb-16">
+                          <a
+                            href="#"
+                            className="inline-flex items-center justify-center rounded-lg bg-green-600 py-3 px-5 text-center text-base font-medium text-white hover:bg-green-800 focus:ring-4 focus:ring-green-300 dark:focus:ring-green-900"
+                          >
+                              Learn more
+                              <svg
+                                className="ml-2 -mr-1 h-5 w-5"
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg"
+                              >
+                                  <path
+                                    fillRule="evenodd"
+                                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                    clipRule="evenodd"
+                                  ></path>
+                              </svg>
+                          </a>
+                      </div>
+                      <div className="mx-auto px-4 text-center md:max-w-screen-md lg:max-w-screen-lg lg:px-36">
+                          {!searchStatus && (
+                            <form
+                              className={"mt-7"}
+                              onSubmit={(e) => {
+                                  e.preventDefault();
+                              }}
+                            >
+                                <div className="mb-6">
+                                    <label
+                                      htmlFor="text"
+                                      className="mb-2 block text-sm font-medium text-gray-500 dark:text-white"
+                                    >
+                                        Waybill Number
+                                    </label>
+                                    <div
+                                      className={
+                                          "flex items-center justify-center space-x-6 text-gray-500"
+                                      }
+                                    >
+                                        <input
+                                          type="number"
+                                          id="email"
+                                          className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 placeholder-gray-400 focus:border-green-500 focus:ring-green-500 dark:border-gray-600 dark:bg-gray-600 dark:text-white dark:placeholder-gray-400 dark:focus:border-green-500 dark:focus:ring-green-500"
+                                          placeholder="1234455677"
+                                        />
                                     </div>
-                                </form>
-                            )}
-                        </div>
-                    </div>
-                </section>
+                                    <button
+                                      onClick={() => {
+                                          setLoading(true);
+                                          setTimeout(() => {
+                                              setSearch(true);
+                                              setLoading(false);
+                                          }, 1500);
+                                      }}
+                                      className="my-5 inline-flex items-center justify-center rounded-lg bg-green-600 py-3 px-5 text-center text-base font-medium text-white hover:bg-green-800 focus:ring-4 focus:ring-green-300 dark:focus:ring-green-900"
+                                    >
+                                        Track
+                                        <svg
+                                          className="ml-2 -mr-1 h-5 w-5"
+                                          fill="currentColor"
+                                          viewBox="0 0 20 20"
+                                          xmlns="http://www.w3.org/2000/svg"
+                                        >
+                                            <path
+                                              fillRule="evenodd"
+                                              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                              clipRule="evenodd"
+                                            ></path>
+                                        </svg>
+                                    </button>
+                                </div>
+                            </form>
+                          )}
+                      </div>
+                  </div>
+              </section>
 
-                <div className={"pt-12 mx-6 mb-20"}>
-                    {loading && (
-                        <svg aria-hidden="true"
-                             className="inline w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-green-600"
-                             viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path
-                                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                                fill="currentColor"/>
-                            <path
-                                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                                fill="currentFill"/>
-                        </svg>
-                    )
-                    }
-                    {(searchStatus && !loading) &&
-                        <TrackingView setSearch={setSearch}/>
-                    }
-                </div>
-            </div>
-        </>
-    )
+              <div className={"mx-6 mb-20 pt-12"}>
+                  {loading && (
+                    <svg
+                      aria-hidden="true"
+                      className="mr-2 inline h-8 w-8 animate-spin fill-green-600 text-gray-200 dark:text-gray-600"
+                      viewBox="0 0 100 101"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                          d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                          fill="currentColor"
+                        />
+                        <path
+                          d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                          fill="currentFill"
+                        />
+                    </svg>
+                  )}
+                  {searchStatus && !loading && <TrackingView setSearch={setSearch} />}
+              </div>
+          </div>
+      </>
+    );
 }
 
 TrackingPage.pageLayout = TabsLayout
