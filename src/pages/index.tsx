c1aa541fbad97c31519553b3df81811ed4c709e5
Prakhar Shukla <imprakharshukla@gmail.com>
2023-01-31 09:54:13 +0530

    add: og tags

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,17 @@
 import Head from 'next/head'
-import {TabsLayout} from "@/layout";
 import {LandingFormView} from "@/components/LandingFormView";
 
 const Home = () => {
     return (
         <>
             <Head>
-                <title>Create Next App</title>
+                <meta property="og:title" content="Giveaid"/>
+                <meta property="og:site_name" content="Giveaid"/>
+                <meta property="og:url" content="https://giveaid.vercel.app"/>
+                <meta property="og:description" content="Donate locally, make a difference globally."/>
+                <meta property="og:type" content="website"/>
+                <meta property="og:image" content="https://giveaid.vercel.app/give_ad.png"/>
+                <title>Giveaid App</title>
                 <meta name="description" content="Next JS Starter"/>
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
                 <link rel="icon" href="/favicon.ico"/>
